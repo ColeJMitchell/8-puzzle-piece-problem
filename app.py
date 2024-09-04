fbaeb992fe380f6018e1245bef95513ffefb5403
ColeJMitchell <colejmitchell.cs@gmail.com>
2024-09-04 13:14:45 -0400

    added randomization of empty square as well as a goal state

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import pygame
 import random
+import heapq
 
 all_squares = [1,2,3,4,5,6,7,8]
-squares = [-1]
+squares = []
 while all_squares:
     choice = random.randint(0,len(all_squares)-1)
     squares.append(all_squares[choice])
     all_squares.remove(all_squares[choice])
+random_insert = random.randint(0,len(squares))
+squares.insert(random_insert,-1)
+original = all_squares
+
+goal_board = [1,2,3,4,5,6,7,8,-1]
+
+
+def calculate_priority():
+    return
 
 def main():
     #set up the graphical interface
@@ -21,10 +31,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-    
+
         grid_size = 300      
         color = (230, 230, 250)
         screen.fill(color)  
+        empty_square = squares.index(-1)
+        empty_row = empty_square // 3
+        empty_column = empty_square % 3
+        adjacent_squares = []
+        if empty_row < 2:
+            adjacent_squares.append(empty_square-3)
+        if empty_row > 0:
+            adjacent_squares.append(empty_square+3)
         #draws the rectangles and places the tile numbers inside of them
         for i, square in enumerate(squares):
             row = i // 3
